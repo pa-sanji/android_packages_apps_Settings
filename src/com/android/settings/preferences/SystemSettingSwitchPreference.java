4b7dc6fc34339733c234cc8b40495e08ecb5fd80
Sanjivns <113604406+Sanjivns@users.noreply.github.com>
2025-11-26 17:34:42 +0000

    as

--- src/com/android/settings/preferences/SystemSettingSwitchPreference.java
+++ src/com/android/settings/preferences/SystemSettingSwitchPreference.java
@@ -20,7 +20,7 @@ import android.provider.Settings;
 import android.os.UserHandle;
 import android.util.AttributeSet;
 
-import com.android.settings.preference.SelfRemovingSwitchPreference;
+import lineageos.preference.SelfRemovingSwitchPreference;
 
 public class SystemSettingSwitchPreference extends SelfRemovingSwitchPreference {
 
